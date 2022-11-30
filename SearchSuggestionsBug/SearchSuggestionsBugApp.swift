0df555ec26839695be055a455a8f995d85c744d8
Jason Ji <uberjason@gmail.com>
2022-11-30 17:49:50 -0500

    Initial Commit

---
+++ SearchSuggestionsBug/SearchSuggestionsBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSuggestionsBugApp.swift
+//  SearchSuggestionsBug
+//
+//  Created by Jason Ji on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchSuggestionsBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
